ac1a31fb5c4b2670dcc992c271f6ec2f01dd3b1f
Anirudh A V <anirudh.av02@gmail.com>
2022-12-18 01:39:50 +0530

    Sidebar theme changed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,18 +17,18 @@ const Sidebar = () => {
 
     return (
         <div className='flex'>
-            <div className={`sidebar w-72 bg-orange-600 flex flex-col h-screen ${sidebar ? "" : "max-lg:hidden"}`}>
+            <div className={`sidebar w-72 bg-blue-700 flex flex-col h-screen ${sidebar ? "" : "max-lg:hidden"}`}>
                 <div className="sidebar__header text-2xl p-2 text-slate-100 font-bold justify-center items-center">
                     <h1>EETI</h1>
                     <h3>CA Management</h3>
                 </div>
                 <div className="sidebar__links flex flex-col mt-6">
-                    <a href="/" className='p-3 text-xl text-slate-100 font-bold hover:bg-orange-400'>Dashboard</a>
-                    <a href="/list" className='p-3 text-xl text-slate-100 font-bold hover:bg-orange-400'>View CA</a>
-                    <a href="/profile" className='p-3 text-xl text-slate-100 font-bold hover:bg-orange-400'>Profile</a>
+                    <a href="/" className='p-3 text-xl text-slate-100 font-bold hover:bg-blue-400'>Dashboard</a>
+                    <a href="/list" className='p-3 text-xl text-slate-100 font-bold hover:bg-blue-400'>View CA</a>
+                    <a href="/profile" className='p-3 text-xl text-slate-100 font-bold hover:bg-blue-400'>Profile</a>
                 </div>
             </div>
-            <div className="m-1 p-2 text-2xl cursor-pointer rounded-3xl hover:shadow-orange-300 hover:shadow-sm h-10">
+            <div className="m-1 p-2 text-2xl cursor-pointer rounded-3xl hover:shadow-blue-300 hover:shadow-sm h-10">
                 <GiHamburgerMenu onClick={showSidebar}  />
             </div>
         </div>
