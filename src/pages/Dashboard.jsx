ef9df1c7eff28914d4f063ae1047f79489b6bb1c
Anirudh A V <anirudh.av02@gmail.com>
2022-12-18 01:14:46 +0530

    Dashboard redirect on unauthorised access

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,49 @@
 import { Header } from "../components"
 import AppWrap from "../containers/Layout"
+import { useState, useEffect } from "react"
+import { db } from "../firebase/config"
+import { collection, getDocs } from "firebase/firestore"
+import { auth } from "../firebase/config"
+import { onAuthStateChanged } from "firebase/auth"
 
 const Dashboard = () => {
+
+  // const [docs, setDocs] = useState([])
+  // const [loading, setLoading] = useState(true)
+  // const [error, setError] = useState(null)
+
+  // const caCollectionRef = collection(db, 'ca')
+
+  // useEffect(() => {
+  //   const getCA = async () => {
+  //     const data = await getDocs(caCollectionRef)
+  //       .catch((err) => {
+  //         setError(err.message)
+  //         setLoading(false)
+  //       })
+  //     console.log(data)
+  //     setDocs(data.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+  //     setLoading(false)
+  //   }
+  //   getCA()
+  // }, [])
+
+  // console.log(docs)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        window.location.href = '/login'
+      }
+    })
+  }, [])
+
   return (
     <div className="flex flex-col m-5">
-        <Header category={'Welcome'} title={'Dashboard'} />
-        
+      <Header category={'Welcome'} title={'Dashboard'} />
+
     </div>
   )
 }
 
-export default Dashboard
+export default AppWrap(Dashboard)
