832eac5ee72c5616b6baa683100e43892afc82dd
Anirudh A V <anirudh.av02@gmail.com>
2022-12-18 10:35:57 +0530

    feat: edit CA page modified
    User verification is added to the edit CA page. If the user is not logged in, he/she will be redirected to the login page. If the user is logged in, but not the owner of the CA, he/she will be redirected to the CA list page.
    AppWrap component removed from the edit CA page. Sidebar component added to the edit CA page.
    Credit field enabled in the edit CA page.
    Bug fixed in the edit CA page. The ref_code and college fields were of the same name. This caused the ref_code field to change its value to the college field's value. This bug is fixed.

--- src/pages/EditCA.jsx
+++ src/pages/EditCA.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-import { db } from '../firebase/config'
+import { db, auth } from '../firebase/config'
 import { collection, getDocs, updateDoc, doc, deleteDoc } from 'firebase/firestore'
 import AppWrap from '../containers/Layout'
-// Component to edit the details of a Campus Ambassador using firefox
+import { onAuthStateChanged } from 'firebase/auth'
+import { Sidebar } from '../components'
 
 const EditCA = () => {
   const { id } = useParams()
@@ -30,14 +31,25 @@ const EditCA = () => {
       const data = await getDocs(caCollectionRef)
         .catch((err) => {
           setError(err.message)
+          console.log(err.message)
           setLoading(false)
         })
       const doc = data.docs.map(doc => ({ ...doc.data(), id: doc.id }))
-      console.log(doc)
+      // console.log(doc)
       setDocs(doc.filter(doc => doc.ref_code === id)[0])
       setLoading(false)
     }
-    getCA()
+
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        // window.location.href = '/login'
+        navigate('/login')
+      } else {
+        getCA()
+      }
+    })
+
+
   }, [])
 
   console.log(docs)
@@ -58,7 +70,7 @@ const EditCA = () => {
     navigate('/list')
   }
 
-  const handleDelete = () =>{
+  const handleDelete = () => {
     const caRef = doc(db, 'ca', docs.id)
     deleteDoc(caRef)
     navigate('/list')
@@ -66,50 +78,52 @@ const EditCA = () => {
 
   if (loading) {
     return (
-        <div className="flex justify-center items-center h-screen w-screen">
-            <div className="flex flex-col justify-center items-center">
-                {/* <h1 className="text-2xl font-bold">Loading...</h1> */}
-                {/* <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-12 w-12"></div> */}
-                <div className="shapes"></div>
-            </div>
+      <div className="flex justify-center items-center h-screen w-screen">
+        <div className="flex flex-col justify-center items-center">
+          {/* <h1 className="text-2xl font-bold">Loading...</h1> */}
+          {/* <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-12 w-12"></div> */}
+          <div className="shapes"></div>
         </div>
+      </div>
     )
-} else if (error) {
+  } else if (error) {
     return (
-        <div className="flex justify-center items-center h-screen w-screen">
-            <div className="flex flex-col justify-center items-center">
-                <h1 className="text-2xl font-bold">Error: {error}</h1>
-            </div>
+      <div className="flex justify-center items-center h-screen w-screen">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-2xl font-bold">Error: {error}</h1>
         </div>
+      </div>
     )
-}
+  }
 
   return (
-    <div className="flex flex-col m-5">
-      <h1 className="text-2xl font-bold">Edit Campus Ambassador</h1>
-      {/* {docs && docs.map(doc => ( */}
-      <form className="flex flex-col">
-        <label className="mt-4">
-          Name
-        </label>
-        <input
-          className="border border-gray-400 p-2 rounded"
-          type="text"
-          name="name"
-          value={docs.name}
-          onChange={handleChange}
-        />
-        <label className="mt-4">
-          Email
-        </label>
-        <input
-          className="border border-gray-400 p-2 rounded"
-          type="email"
-          name="email"
-          value={docs.email}
-          onChange={handleChange}
-        />
-        {/* <label className="mt-4">
+    <div className='flex'>
+      <Sidebar />
+      <div className="flex flex-col m-5">
+        <h1 className="text-2xl font-bold">Edit Campus Ambassador</h1>
+        {/* {docs && docs.map(doc => ( */}
+        <form className="flex flex-col">
+          <label className="mt-4">
+            Name
+          </label>
+          <input
+            className="border border-gray-400 p-2 rounded"
+            type="text"
+            name="name"
+            value={docs.name}
+            onChange={handleChange}
+          />
+          <label className="mt-4">
+            Email
+          </label>
+          <input
+            className="border border-gray-400 p-2 rounded"
+            type="email"
+            name="email"
+            value={docs.email}
+            onChange={handleChange}
+          />
+          {/* <label className="mt-4">
           Phone
         </label>
           <input
@@ -118,51 +132,52 @@ const EditCA = () => {
             name="phone"
             value={doc.phone}
           /> */}
-        <label className="mt-4">
-          College
-        </label>
-        <input
-          className="border border-gray-400 p-2 rounded"
-          type="text"
-          name="college"
-          value={docs.college}
-          onChange={handleChange}
-        />
-        <label className="mt-4">
-          Referral Code
-        </label>
-        <input
-          className="border border-gray-400 p-2 rounded"
-          type="text"
-          name="college"
-          value={docs.ref_code}
-          onChange={handleChange}
-        />
-        <label className="mt-4">
-          Credits
-        </label>
-        <input
-          className="border border-gray-400 p-2 rounded"
-          type="number"
-          name="credit"
-          disabled
-          value={docs.credit}
-        />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4  ml-2 mr-2" onClick={handleSubmit}>
-          Submit
-        </button>
-        <div className='flex flex-row justify-around '>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4 w-full ml-2 mr-2 " onClick={() => navigate('/list')}>
-            Back
+          <label className="mt-4">
+            College
+          </label>
+          <input
+            className="border border-gray-400 p-2 rounded"
+            type="text"
+            name="college"
+            value={docs.college}
+            onChange={handleChange}
+          />
+          <label className="mt-4">
+            Referral Code
+          </label>
+          <input
+            className="border border-gray-400 p-2 rounded"
+            type="text"
+            name="ref_code"
+            value={docs.ref_code}
+            onChange={handleChange}
+          />
+          <label className="mt-4">
+            Credits
+          </label>
+          <input
+            className="border border-gray-400 p-2 rounded"
+            type="number"
+            name="credit"
+            value={docs.credit}
+            onChange={handleChange}
+          />
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4  ml-2 mr-2" onClick={handleSubmit}>
+            Submit
           </button>
-          <button className="border border-red-600 hover:bg-red-700 hover:text-white text-red-600 font-bold py-2 px-4 rounded mt-4 w-full ml-2 mr-2" onClick={handleDelete}>
-            Delete
-          </button>
-        </div>
-      </form>
-      {/* ))} */}
+          <div className='flex flex-row justify-around '>
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4 w-full ml-2 mr-2 " onClick={() => navigate('/list')}>
+              Back
+            </button>
+            <button className="border border-red-600 hover:bg-red-700 hover:text-white text-red-600 font-bold py-2 px-4 rounded mt-4 w-full ml-2 mr-2" onClick={handleDelete}>
+              Delete
+            </button>
+          </div>
+        </form>
+        {/* ))} */}
+      </div>
     </div>
   )
 }
 
-export default AppWrap(EditCA)
+export default EditCA
