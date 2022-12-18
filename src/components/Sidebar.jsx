dcbae29681e2970037c45f24f937e85861bfa801
Anirudh A V <anirudh.av02@gmail.com>
2022-12-18 10:40:28 +0530

    feat: Sidebar updated
    Sidebar is shown only when the window width is greater than 900px. Otherwise, it is hidden and a hamburger menu is shown. When the hamburger menu is clicked, the sidebar is shown.
    Sidebar min-height is set to 100vh and height is set to 100%. This is done to make sure that the sidebar is always shown completely even when the content is less than the height of the window.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,22 +2,28 @@ import { useEffect, useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 const Sidebar = () => {
     const [sidebar, setSidebar] = useState(true)
+    // const
 
-    // if screen width is less than 768px, sidebar will be hidden
+    // if screen width is less than 900px, sidebar will be hidden
     useEffect(() => {
-        if (window.innerWidth < 768) {
+        if (window.innerWidth < 900) {
             setSidebar(false)
+            // console.log(sidebar)
+            // console.log("Sidebar is hidden")
         } else {
             setSidebar(true)
         }
     }, [])
 
 
-    const showSidebar = () => setSidebar(!sidebar)
+    const showSidebar = () => {
+        setSidebar(!sidebar)
+        // sidebar ? console.log("Sidebar is hidden") : console.log("Sidebar is shown")
+    }
 
     return (
         <div className='flex'>
-            <div className={`sidebar w-72 bg-blue-700 flex flex-col h-screen ${sidebar ? "" : "max-lg:hidden"}`}>
+            <div className={`sidebar w-72 bg-blue-700 flex flex-col min-h-screen h-full ${sidebar ? "" : "max-lg:hidden"}`}>
                 <div className="sidebar__header text-2xl p-2 text-slate-100 font-bold justify-center items-center">
                     <h1>EETI</h1>
                     <h3>CA Management</h3>
@@ -29,7 +35,7 @@ const Sidebar = () => {
                 </div>
             </div>
             <div className="m-1 p-2 text-2xl cursor-pointer rounded-3xl hover:shadow-blue-300 hover:shadow-sm h-10">
-                <GiHamburgerMenu onClick={showSidebar}  />
+                <GiHamburgerMenu onClick={showSidebar} />
             </div>
         </div>
     )
